de442247f80b60167c3c4e55bc7e3ca052412b4e
Swapnil Bhavsar <swapnil@swapnil.co>
2018-03-25 17:55:34 +0530

    Refactore header.php

--- header.php
+++ header.php
@@ -26,10 +26,9 @@
     <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'camel' ); ?></a>
     <header id="masthead" class="site-header">
         <div class="site-branding">
-            <?php
-                the_custom_logo();
-                if ( is_front_page() && is_home() ) :
-            ?>
+            <?php the_custom_logo(); ?>
+
+            <?php if ( is_front_page() && is_home() ) : ?>
                 <h1 class="site-title">
                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
                 </h1>
@@ -39,10 +38,9 @@
                 </p>
             <?php endif; ?>
 
-            <?php
-                $bloginfo = get_bloginfo( 'description', 'display' );
-                if ( $bloginfo || is_customize_preview() ) :
-            ?>
+            <?php $bloginfo = get_bloginfo( 'description', 'display' ); ?>
+
+            <?php if ( $bloginfo || is_customize_preview() ) : ?>
                 <p class="site-description"><?php echo $bloginfo; ?></p>
             <?php endif; ?>
         </div><!-- .site-branding -->
