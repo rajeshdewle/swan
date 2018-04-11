f570869e9f801ed508659ec3ff4a5bfefd4926ff
Swapnil Bhavsar <swapnil@swapnil.co>
2018-04-11 09:50:06 +0530

    Remove bootstrap navwalker entry

--- core/camel.php
+++ core/camel.php
@@ -1,8 +1,5 @@
 <?php
 
-// Bootrsap Nav Walker
-require_once get_template_directory() . '/core/inc/class-wp-bootstrap-nav-walker.php';
-
 // Camel Nav Walker
 require_once get_template_directory() . '/core/inc/class-camel-nav-walker.php';
 
