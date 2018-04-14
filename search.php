f3384391b206b686871bb471e5f8c3138006afae
Rajesh Dewle <rajesh_dewle@hotmail.com>
2018-04-14 11:25:11 +0530

    Add content.none.php

--- search.php
+++ search.php
@@ -50,7 +50,7 @@ get_header();
 
         else :
 
-            get_template_part( 'template-parts/content', 'none' );
+            get_template_part( 'template-parts/post/content', 'none' );
 
         endif;
         ?>
