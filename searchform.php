0da68c05d22b90b94416affce54faa1aa82a24a1
Swapnil Bhavsar <swapnil@swapnil.co>
2018-04-17 16:54:11 +0530

    Fixed search form layout

--- searchform.php
+++ searchform.php
@@ -11,9 +11,11 @@
 
 ?>
 <form role="search" method="get" class="form-inline" action="<?php echo home_url( '/' ); ?>">
-    <label class="sr-only"><?php echo _x( 'Search for:', 'label', 'camel-framework' ) ?></label>
-    <input type="search" class="form-control mr-sm-2 mb-2" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder', 'camel-framework' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'camel-framework' ) ?>" />
-    <button type="submit" class="btn btn-outline-primary my-2 my-sm-0">
+    <div class="form-group mb-2 mr-2">
+        <label class="sr-only"><?php echo _x( 'Search for:', 'label', 'camel-framework' ) ?></label>
+    <input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder', 'camel-framework' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'camel-framework' ) ?>" />
+    </div>
+    <button type="submit" class="btn btn-outline-primary mb-2">
         <?php echo esc_attr_x( 'Search', 'submit button', 'camel-framework' ) ?>
     </button>
 </form>
