99ee90b8f256bbd4d6eec3e7c9f8fade29615caf
Swapnil Bhavsar <swapnil@swapnil.co>
2018-04-17 16:44:46 +0530

    Fixed search page layout

--- search.php
+++ search.php
@@ -19,7 +19,7 @@ get_header();
         <?php
         if ( have_posts() ) : ?>
 
-            <header class="page-header">
+            <header class="page-header mb-4">
                 <h1 class="page-title">
                     <?php
                     /* translators: %s: search query. */
@@ -28,28 +28,26 @@ get_header();
                 </h1>
             </header><!-- .page-header -->
 
-            <?php
-            /* Start the Loop */
-            while ( have_posts() ) :
-                the_post();
+            <div class="row">
+                    <?php while (have_posts()) :  the_post(); ?>
+                        <?php
+                            /*
+                             * Include the Post-Type-specific template for the content.
+                             * If you want to override this in a child theme, then include a file
+                             * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                             */
+                            get_template_part('template-parts/post/content', get_post_type());
+                        ?>
+                    <?php endwhile; ?>
+                </div><!-- .row -->
 
-                /**
-                 * Run the loop for the search to output the results.
-                 * If you want to overload this in a child theme then include a file
-                 * called content-search.php and that will be used instead.
-                 */
-                get_template_part('template-parts/post/content', get_post_type());
+                <?php camel_posts_pagination(); ?>
 
-            endwhile;
+            <?php else : ?>
 
-            camel_posts_pagination();
+                <?php  get_template_part('template-parts/post/content', 'none'); ?>
 
-        else :
-
-            get_template_part( 'template-parts/post/content', 'none' );
-
-        endif;
-        ?>
+            <?php endif; ?>
         </main><!-- #main -->
 
 
