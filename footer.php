e5e983e95539a230110bec9199a4462e84e08b8b
Sumit Wagh <sumit_wagh@hotmail.com>
2018-04-19 18:06:08 +0530

    Footer copyright section improvement

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 
     <footer class="footer bg-light border-top mt-4">
         <div class="container">
-
+        
             <?php if (has_nav_menu('menu-footer')) :  ?>
                 <div class="footer-menu my-4">
                     <?php get_template_part('template-parts/navigation/navigation', 'footer'); ?>
@@ -28,8 +28,10 @@
                 <a href="<?php echo esc_url(__('https://wordpress.org/', 'camel-framework')); ?>">
                     <?php printf(esc_html__('Proudly powered by %s', 'camel-framework'), 'WordPress'); ?>
                 </a>
-                <span class="sep"> | </span>
-                <?php printf(esc_html__('Theme: %1$s by %2$s.', 'camel-framework'), 'camel-framework', '<a href="#">CamelThemes</a>'); ?>
+                <span class="sep d-sm-inline-block d-none"> | </span>
+                    <div class="d-inline-block">
+                    <?php printf(esc_html__('Theme: %1$s by %2$s.', 'camel-framework'), 'camel-framework', '<a href="#">CamelThemes</a>'); ?>
+                    </div>
             </div><!-- .copyright -->
 
         </div><!-- .container -->
