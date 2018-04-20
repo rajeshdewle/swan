db614fe3365d0264b96d506f9b6561a9e6c3e9db
Rajesh Dewle <rajesh_dewle@hotmail.com>
2018-04-20 18:04:03 +0530

    Add site title/description text color control from customizer

--- functions.php
+++ functions.php
@@ -79,7 +79,7 @@ if (! function_exists('camel_setup')) :
         add_theme_support('customize-selective-refresh-widgets');
 
         // Set up the WordPress core custom background feature.
-        add_theme_support('custom-background', apply_filters('_s_custom_background_args', array(
+        add_theme_support('custom-background', apply_filters('camel_custom_background_args', array(
             'default-color' => 'ffffff',
             'default-image' => '',
         )));
@@ -107,7 +107,7 @@ if (! function_exists('camel_setup')) :
             'height'             => 250,
             'flex-width'         => true,
             'flex-height'        => true,
-            'wp-head-callback'           => 'camel_header_style',
+            'wp-head-callback'   => 'camel_header_style',
         ));
     }
 endif; // camel_setup
@@ -115,19 +115,36 @@ endif; // camel_setup
 add_action('after_setup_theme', 'camel_setup');
 
 if (! function_exists('camel_header_style')) {
-
     function camel_header_style() {
-        if (! display_header_text()) : ?>
-            <style>
-                .site-title,
-                .site-description {
-                    position: absolute;
-                    clip: rect(1px, 1px, 1px, 1px);
-                }
-            </style>
-        <?php endif;
+        $header_text_color = get_header_textcolor();
+        if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
+            return;
+        }
+
+        // If we get this far, we have custom styles. Let's do this.
+        ?>
+        <style type="text/css">
+        <?php
+        // Has the text been hidden?
+        if ( ! display_header_text() ) :
+            ?>
+            .site-title,
+            .site-description {
+                position: absolute;
+                clip: rect(1px, 1px, 1px, 1px);
+            }
+        <?php
+        // If the user has set a custom color for the text use that.
+        else :
+            ?>
+            .site-title a,
+            .site-description {
+                color: #<?php echo esc_attr( $header_text_color ); ?>;
+            }
+        <?php endif; ?>
+        </style>
+        <?php
     }
-    
 }
 
 /**
