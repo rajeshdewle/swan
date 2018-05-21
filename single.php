5dcaf9c0448e734341ac10f2b7b4ce476f4fbe4c
Rajesh Dewle <rajesh_dewle@hotmail.com>
2018-05-21 18:34:38 +0530

    Add content-image.php for image post format

--- single.php
+++ single.php
@@ -24,7 +24,7 @@ get_header();
 
                 the_post();
 
-                get_template_part( 'template-parts/post/content', get_post_type() );
+                get_template_part( 'template-parts/post/content', get_post_format() );
 
                 camel_post_navigation();
 
