c911f14a167d8a42342ed5205e70b61d1ca870f5
Swapnil Bhavsar <swapnil@swapnil.co>
2018-06-08 17:16:02 +0530

    Revert "Fixed widgets default title and description translatable."

--- template-parts/widget/widget-about-us.php
+++ template-parts/widget/widget-about-us.php
@@ -27,9 +27,10 @@ class About_Us_Widget extends WP_Widget {
 
         extract($args);
 
-        $title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'About Us', 'camel-framework' );
+        $image = ! empty( $instance['image'] ) ? $instance['image'] : '';
+		$title = apply_filters('widget_title', $instance['title']);
 		$image =  $instance['image'];
-        $about_textarea = ( ! empty( $instance['about_textarea'] ) ) ? $instance['about_textarea'] : __( 'Camel framework is a beautiful open source WordPress Framework by Camel Team.', 'camel-framework' );
+		$about_textarea =  $instance['about_textarea'];
 		$about_url =  $instance['about_url'];
 
 
@@ -63,9 +64,7 @@ class About_Us_Widget extends WP_Widget {
 		}
 
 		if ($about_url) {
-			echo '<a href="'. $about_url .'" class="footer-about-lmore">';
-            _e( 'Learn more', 'camel-framework');
-            echo '</a>';
+			echo '<a href="'. $about_url .'" class="footer-about-lmore">Learn more</a>';
 		}
 
 		echo "</div>";
@@ -79,7 +78,9 @@ class About_Us_Widget extends WP_Widget {
         $image = ! empty( $instance['image'] ) ? $instance['image'] : '';
 
         $defaults = array(
+			'title' => __( 'About Us', 'camel-framework' ),
 			'image' => site_url().'/wp-content/themes/camel-framework/assets/images/camel-logo.svg',
+			'about_textarea' => __('Camel framework is a beautiful open source WordPress Framework by Camel Team.', 'camel-framework'),
 			'about_url' => '#',
          );
 
