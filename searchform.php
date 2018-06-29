c66f4fe1b51082bb7c64729c36c36a722d04273a
Sumit Wagh <sumit_wagh@hotmail.com>
2018-06-29 12:52:58 +0530

    Add product search form with boostrap classes.
    - Added product search form in woocommerce directory.
    - Improve blog search form.
    - Refactoring dockblock.

--- searchform.php
+++ searchform.php
@@ -10,7 +10,7 @@
  */
 
 ?>
-<form role="search" method="get" class="form-inline" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
     <div class="form-group mb-2 mr-2">
         <label class="sr-only"><?php echo _x( 'Search for:', 'label', 'camel-framework' ) ?></label>
         <input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder', 'camel-framework' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'camel-framework' ) ?>" />
