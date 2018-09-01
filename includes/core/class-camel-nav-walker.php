d725122ee00319667cdb04cdc5d96984bae071a1
Swapnil Bhavsar <swapnil@swapnil.co>
2018-09-01 16:27:56 +0530

    Added home icon in nav walker

--- includes/core/class-camel-nav-walker.php
+++ includes/core/class-camel-nav-walker.php
@@ -7,8 +7,119 @@
  * @package Camel_Framework
  */
 
-if ( ! class_exists( 'Camel_Navwalker' ) ) {
-    class Camel_Navwalker extends Walker_Nav_Menu {
+if (! class_exists('Camel_Navwalker')) {
+    class Camel_Navwalker extends Walker_Nav_Menu
+    {
+        /**
+         * Display array of elements hierarchically.
+         *
+         * Does not assume any existing order of elements.
+         *
+         * $max_depth = -1 means flatly display every element.
+         * $max_depth = 0 means display all levels.
+         * $max_depth > 0 specifies the number of display levels.
+         *
+         * @since 2.1.0
+         *
+         * @param array $elements  An array of elements.
+         * @param int   $max_depth The maximum hierarchical depth.
+         * @return string The hierarchical item output.
+         */
+        public function walk($elements, $max_depth)
+        {
+            $args = array_slice(func_get_args(), 2);
+            $output = '';
+
+            $argsArray = (array) $args[0];
+
+            if ($argsArray['has_home_icon']) {
+                $link_classes = [];
+                $link_classes[] = ! empty($argsArray['link_class']) ? $argsArray['link_class']  : '';
+                $link_classes[] = ($this->has_children) ? 'dropdown-toggle' : '';
+                $linkClasses = implode(' ', $link_classes);
+
+                $list_Classes[] = 'menu-item';
+                if (is_home()) {
+                    $list_Classes[] = 'active';
+                }
+                $listClasses = implode(' ', $list_Classes);
+                $home_icon = isset($argsArray['home_icon']) ? $argsArray['home_icon'] : '';
+
+                $output .= '<li class="' . $listClasses . '"><a class="' . $linkClasses . '" href="/">
+                    ' . $home_icon . '
+                </a></li>';
+            }
+
+            //invalid parameter or nothing to walk
+            if ($max_depth < -1 || empty($elements)) {
+                return $output;
+            }
+
+            $parent_field = $this->db_fields['parent'];
+
+            // flat display
+            if (-1 == $max_depth) {
+                $empty_array = [];
+                foreach ($elements as $e) {
+                    $this->display_element($e, $empty_array, 1, 0, $args, $output);
+                }
+                return $output;
+            }
+
+            /*
+            * Need to display in hierarchical order.
+            * Separate elements into two buckets: top level and children elements.
+            * Children_elements is two dimensional array, eg.
+            * Children_elements[10][] contains all sub-elements whose parent is 10.
+            */
+            $top_level_elements = [];
+            $children_elements  = [];
+            foreach ($elements as $e) {
+                if (empty($e->$parent_field)) {
+                    $top_level_elements[] = $e;
+                } else {
+                    $children_elements[ $e->$parent_field ][] = $e;
+                }
+            }
+
+            /*
+            * When none of the elements is top level.
+            * Assume the first one must be root of the sub elements.
+            */
+            if (empty($top_level_elements)) {
+                $first = array_slice($elements, 0, 1);
+                $root = $first[0];
+
+                $top_level_elements = [];
+                $children_elements  = [];
+                foreach ($elements as $e) {
+                    if ($root->$parent_field == $e->$parent_field) {
+                        $top_level_elements[] = $e;
+                    } else {
+                        $children_elements[ $e->$parent_field ][] = $e;
+                    }
+                }
+            }
+
+            foreach ($top_level_elements as $e) {
+                $this->display_element($e, $children_elements, $max_depth, 0, $args, $output);
+            }
+
+            /*
+            * If we are displaying all levels, and remaining children_elements is not empty,
+            * then we got orphans, which should be displayed regardless.
+            */
+            if (($max_depth == 0) && count($children_elements) > 0) {
+                $empty_array = [];
+                foreach ($children_elements as $orphans) {
+                    foreach ($orphans as $op) {
+                        $this->display_element($op, $empty_array, 1, 0, $args, $output);
+                    }
+                }
+            }
+
+            return $output;
+        }
 
         /**
          * Starts the list before the elements are added.
@@ -21,20 +132,22 @@ if ( ! class_exists( 'Camel_Navwalker' ) ) {
          * @param int      $depth  Depth of menu item. Used for padding.
          * @param stdClass $args   An object of wp_nav_menu() arguments.
          */
-        public function start_lvl( &$output, $depth = 0, $args = array() ) {
-            if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+        public function start_lvl(&$output, $depth = 0, $args = [])
+        {
+            if (isset($args->item_spacing) && 'discard' === $args->item_spacing) {
                 $t = '';
                 $n = '';
             } else {
                 $t = "\t";
                 $n = "\n";
             }
-            $indent = str_repeat( $t, $depth );
+            $indent = str_repeat($t, $depth);
 
             // Default class.
             $argsArray = (array) $args;
+
             $subMenuClass = ! empty($argsArray['submenu_class']) ? $argsArray['submenu_class'] : '';
-            $classes = array( 'sub-menu' );
+            $classes = [ 'sub-menu' ];
             $classes[] = $subMenuClass;
 
             /**
@@ -46,8 +159,8 @@ if ( ! class_exists( 'Camel_Navwalker' ) ) {
              * @param stdClass $args    An object of `wp_nav_menu()` arguments.
              * @param int      $depth   Depth of menu item. Used for padding.
              */
-            $class_names = join( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
-            $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+            $class_names = join(' ', apply_filters('nav_menu_submenu_css_class', $classes, $args, $depth));
+            $class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
 
             $output .= "{$n}{$indent}<ul$class_names aria-labelledby=\"sub-menu\">{$n}";
         }
@@ -66,21 +179,22 @@ if ( ! class_exists( 'Camel_Navwalker' ) ) {
          * @param stdClass $args   An object of wp_nav_menu() arguments.
          * @param int      $id     Current item ID.
          */
-        public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-            if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+        public function start_el(&$output, $item, $depth = 0, $args = [], $id = 0)
+        {
+            if (isset($args->item_spacing) && 'discard' === $args->item_spacing) {
                 $t = '';
                 $n = '';
             } else {
                 $t = "\t";
                 $n = "\n";
             }
-            $indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
+            $indent = ($depth) ? str_repeat($t, $depth) : '';
 
             // Apply list classes to 'li' element
             $argsArray = (array) $args;
             $item->classes[] = ! empty($argsArray['list_class']) ? $argsArray['list_class'] : '';
 
-            $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+            $classes = empty($item->classes) ? [] : (array) $item->classes;
             $classes[] = 'menu-item-' . $item->ID;
 
 
@@ -93,15 +207,15 @@ if ( ! class_exists( 'Camel_Navwalker' ) ) {
              * @param WP_Post  $item  Menu item data object.
              * @param int      $depth Depth of menu item. Used for padding.
              */
-            $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
+            $args = apply_filters('nav_menu_item_args', $args, $item, $depth);
 
 
             // Add .dropdown or .active classes where they are needed.
-            if ( $this->has_children ) {
+            if ($this->has_children) {
                 $classes[] = 'dropdown';
             }
 
-            if ( in_array( 'current-menu-item', $classes, true ) || in_array( 'current-menu-parent', $classes, true ) ) {
+            if (in_array('current-menu-item', $classes, true) || in_array('current-menu-parent', $classes, true)) {
                 $classes[] = 'active';
             }
 
@@ -116,8 +230,8 @@ if ( ! class_exists( 'Camel_Navwalker' ) ) {
              * @param stdClass $args    An object of wp_nav_menu() arguments.
              * @param int      $depth   Depth of menu item. Used for padding.
              */
-            $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
-            $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+            $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args, $depth));
+            $class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
 
             /**
              * Filters the ID applied to a menu item's list item element.
@@ -130,21 +244,21 @@ if ( ! class_exists( 'Camel_Navwalker' ) ) {
              * @param stdClass $args    An object of wp_nav_menu() arguments.
              * @param int      $depth   Depth of menu item. Used for padding.
              */
-            $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
-            $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+            $id = apply_filters('nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth);
+            $id = $id ? ' id="' . esc_attr($id) . '"' : '';
 
             $output .= $indent . '<li' . $id . $class_names .'>';
 
-            $link_classes = array();
-            $link_classes[] = ! empty( $argsArray['link_class'] ) ? $argsArray['link_class']  : '';
+            $link_classes = [];
+            $link_classes[] = ! empty($argsArray['link_class']) ? $argsArray['link_class']  : '';
             $link_classes[] = ($this->has_children) ? 'dropdown-toggle' : '';
 
-            $atts = array();
-            $atts['title']  = ! empty( $item->attr_title )  ? $item->attr_title : '';
-            $atts['target'] = ! empty( $item->target )      ? $item->target     : '';
-            $atts['rel']    = ! empty( $item->xfn )         ? $item->xfn        : '';
-            $atts['href']   = ! empty( $item->url )         ? $item->url        : '';
-            $atts['class']  = ! empty( $link_classes )       ?  implode(' ', $link_classes)     : '';
+            $atts = [];
+            $atts['title']  = ! empty($item->attr_title)  ? $item->attr_title : '';
+            $atts['target'] = ! empty($item->target)      ? $item->target     : '';
+            $atts['rel']    = ! empty($item->xfn)         ? $item->xfn        : '';
+            $atts['href']   = ! empty($item->url)         ? $item->url        : '';
+            $atts['class']  = ! empty($link_classes)       ?  implode(' ', $link_classes)     : '';
 
 
             if ($this->has_children) {
@@ -172,18 +286,18 @@ if ( ! class_exists( 'Camel_Navwalker' ) ) {
              * @param stdClass $args  An object of wp_nav_menu() arguments.
              * @param int      $depth Depth of menu item. Used for padding.
              */
-            $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+            $atts = apply_filters('nav_menu_link_attributes', $atts, $item, $args, $depth);
 
             $attributes = '';
-            foreach ( $atts as $attr => $value ) {
-                if ( ! empty( $value ) ) {
-                    $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+            foreach ($atts as $attr => $value) {
+                if (! empty($value)) {
+                    $value = ('href' === $attr) ? esc_url($value) : esc_attr($value);
                     $attributes .= ' ' . $attr . '="' . $value . '"';
                 }
             }
 
             /** This filter is documented in wp-includes/post-template.php */
-            $title = apply_filters( 'the_title', $item->title, $item->ID );
+            $title = apply_filters('the_title', $item->title, $item->ID);
 
             /**
              * Filters a menu item's title.
@@ -195,7 +309,7 @@ if ( ! class_exists( 'Camel_Navwalker' ) ) {
              * @param stdClass $args  An object of wp_nav_menu() arguments.
              * @param int      $depth Depth of menu item. Used for padding.
              */
-            $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+            $title = apply_filters('nav_menu_item_title', $title, $item, $args, $depth);
 
             $item_output = $args->before;
             $item_output .= '<a'. $attributes .'>';
@@ -217,7 +331,7 @@ if ( ! class_exists( 'Camel_Navwalker' ) ) {
              * @param int      $depth       Depth of menu item. Used for padding.
              * @param stdClass $args        An object of wp_nav_menu() arguments.
              */
-            $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+            $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
         }
     }
 }
